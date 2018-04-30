beddb49ed38f5cd5c1e976868ab855f5585af843
Jellyfish <turtle20953809@gmail.com>
2018-04-30 11:26:51 +0800

    owo

--- 0430/mysite/polls/urls.py
+++ 0430/mysite/polls/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^search_form/$', views.search_form, name='search_form'),
     url(r'^search/$', views.search, name="search"),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 ]
--- 0430/mysite/polls/views.py
+++ 0430/mysite/polls/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+
+from .models import Choice, Question
 
 def search_form(request):
 	return render(request, 'search_form.html')
@@ -8,3 +11,33 @@ def search(request):
 	request.encoding = 'utf-8'
 	message = 'What you fill in is: ' + request.GET.get('q')
 	return HttpResponse(message)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'index.html', {'latest_question_list': latest_question_list})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
